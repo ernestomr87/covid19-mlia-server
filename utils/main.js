3173342e29ca2bedac735e54cd10a83d236ff43b
Leo Perez <leoperezortiz@gmail.com>
2020-11-19 17:22:07 -0600

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -92,7 +92,7 @@ const asyncForEachRunScript = async (files) => {
     //   const element = array[i];
     // }
 
-    const script = `bash eval_competition/covid19mlia-mt-evaluation/calc_scores.sh eval_competition/covid19mlia-mt-evaluation/tests/en-es/ref_test_enes.es.sgm eval_competition/covid19mlia-mt-evaluation/pangeanic_task3_round1_en2es_constrained_prueba.sgm`;
+    const script = `bash /home/ubuntu/eval_competition/covid19mlia-mt-evaluation/calc_scores.sh /home/ubuntu/eval_competition/covid19mlia-mt-evaluation/tests/en-es/ref_test_enes.es.sgm /home/ubuntu/eval_competition/covid19mlia-mt-evaluation/pangeanic_task3_round1_en2es_constrained_prueba.sgm`;
     const myShellScript = exec(script);
     myShellScript.stdout.on("data", (data) => {
       console.log(data);
