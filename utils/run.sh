55f3903e4f8b53937aae7fb7ae69251c0a869cb5
Leo Perez <leoperezortiz@gmail.com>
2020-11-20 17:28:12 -0600

    Update run.sh

--- utils/run.sh
+++ utils/run.sh
@@ -6,9 +6,9 @@ REPO=$2
 
 cd /home/ubuntu/covid-19/covid19-mlia-server/repos/$FOLDER
 git config --local user.email "m.garcia@pangeanic.com"
-git config --local user.name 'm.garcia'
+git config --local user.name 'mgarciamartinez'
 git config --local user.password 'Pangeanic2020!'
-git remote set-url origin https://m.garcia:Pangeanic2020!@$REPO
+git remote set-url origin https://mgarciamartinez:Pangeanic2020!@$REPO
 git add .
 git commit -a -m "commit"
 git push
