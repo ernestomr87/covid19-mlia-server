4e81f0c26c3d4c34b7c38df42854b322b78062d5
Leo Perez <leoperezortiz@gmail.com>
2020-11-20 17:03:43 -0600

    Update run.sh

--- utils/run.sh
+++ utils/run.sh
@@ -4,8 +4,14 @@
 FOLDER=$1
 
 cd /home/ubuntu/covid-19/covid19-mlia-server/repos/$FOLDER
+git config --local user.email "m.garcia@pangeanic.com"
+git config --local user.name 'm.garcia'
+git config --local user.password 'Pangeanic2020!'
 git add .
 git commit -a -m "commit"
 git push
+
+
+
 
 
